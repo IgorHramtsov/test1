568180575dbca4d9fbcb5e44d6c203caa0ba918f
IgorHramtsov <hramtsov2901@gmail.com>
2022-12-03 15:09:00 +0200

    Class 1 - Create a methods

--- src/main/java/Class1.java
+++ src/main/java/Class1.java
@@ -1,6 +1,13 @@
 public class Class1 {
     public static void main(String[] args) {
         System.out.println("Hello World");
+    }
+
+    public static void a1(String[] args) {
+
+    }
+
+    public static void a2(String[] args) {
 
     }
 }
